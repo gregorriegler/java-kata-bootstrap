a6b2277cb187dfdbad197f6a70419ad43bf97217
Gregor Riegler <gregor.riegler@ebcont.com>
2019-09-22 00:56:52 +0200

    refactor: rename

--- src/test/java/GolTest.java
+++ src/test/java/GolTest.java
@@ -1,24 +1,18 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import java.util.stream.IntStream;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-class HelloWorldTest {
 
-    private Broadcaster broadcaster;
+class GolTest {
+
+    private Broadcaster broadcaster = mock(Broadcaster.class);
     private Cell cell;
 
-    @BeforeEach
-    void setUp() {
-        broadcaster = Mockito.mock(Broadcaster.class);
-    }
-
     @Test
-    void
-    cellPublishesInitialState() {
+    void cellPublishesInitialState() {
         givenADeadCell();
         cell.onEvent(new NewGenerationEvent());
         verify(broadcaster).broadcast(new LifeStateEvent(false));
@@ -38,6 +32,16 @@ class HelloWorldTest {
         thenCellShouldReportDeathInNextGeneration();
     }
 
+    private void thenCellShouldReportAliveInNextGeneration() {
+        cell.onEvent(new NewGenerationEvent());
+        verify(broadcaster).broadcast(new LifeStateEvent(true));
+    }
+
+    private void thenCellShouldReportDeathInNextGeneration() {
+        cell.onEvent(new NewGenerationEvent());
+        verify(broadcaster).broadcast(new LifeStateEvent(false));
+    }
+
     private void givenALivingCell() {
         cell = new Cell(broadcaster, true);
     }
@@ -50,16 +54,5 @@ class HelloWorldTest {
         IntStream.range(0, count)
             .forEach(i -> cell.onEvent(new LifeStateEvent(true)));
     }
-
-    private void thenCellShouldReportAliveInNextGeneration() {
-        cell.onEvent(new NewGenerationEvent());
-        verify(broadcaster).broadcast(new LifeStateEvent(true));
-    }
-
-    private void thenCellShouldReportDeathInNextGeneration() {
-        cell.onEvent(new NewGenerationEvent());
-        verify(broadcaster).broadcast(new LifeStateEvent(false));
-    }
-
 }
 
