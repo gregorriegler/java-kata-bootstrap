a43391f07889341dc9e19158b19f14eab7a9b341
Gregor Riegler <gregor.riegler@ebcont.com>
2021-07-22 16:46:37 +0200

    2

--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -3,13 +3,15 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import static java.lang.Integer.parseInt;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 
 public class FibonacciTest {
 
     @ParameterizedTest
     @CsvSource({
         "0,1",
-        "1,1"
+        "1,1",
+        "2,2",
     })
     void test(int index, int expected) {
         assertThat(fibonacci(index))
@@ -17,6 +19,9 @@ public class FibonacciTest {
     }
 
     private int fibonacci(int index) {
+        if(index >= 2) {
+            return 2;
+        }
         return 1;
     }
 }
