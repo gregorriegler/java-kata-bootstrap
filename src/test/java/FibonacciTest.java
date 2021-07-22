dc9f6608ffe0bd867ae4fce467cd7323e89568e1
Gregor Riegler <gregor.riegler@ebcont.com>
2021-07-22 16:42:36 +0200

    0

---
+++ src/test/java/FibonacciTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static java.lang.Integer.parseInt;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FibonacciTest {
+
+    @ParameterizedTest
+    @CsvSource({
+        "0,1"
+    })
+    void test(int index, int expected) {
+        assertThat(fibonacci(index))
+            .isEqualTo(expected);
+    }
+
+    private int fibonacci(int index) {
+        return 1;
+    }
+}
