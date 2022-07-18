0de4799b3e21d5e7c62bfcab397751a6a523ca0f
Gregor Riegler <gregor.riegler@ebcont.com>
2022-07-18 22:56:09 +0200

    wip

--- src/test/java/tdd/MarsRover.java
+++ src/test/java/tdd/MarsRover.java
@@ -1,2 +1,25 @@
-package tdd;public class MarsRover {
+package tdd;
+
+class MarsRover {
+
+    private Direction facing = Direction.NORTH;
+
+    public void sendCommands(String commands) {
+
+        for (char command : commands.toCharArray()) {
+            if (command == 'r') {
+                facing = Direction.EAST;
+            }
+
+        }
+
+    }
+
+    public Direction facing() {
+        return facing;
+    }
+
+    public Position position() {
+        return new Position(0, 0);
+    }
 }
--- src/test/java/tdd/MarsRoverTest.java
+++ src/test/java/tdd/MarsRoverTest.java
@@ -30,27 +30,4 @@ public class MarsRoverTest {
         assertThat(rover.position()).isEqualTo(new Position(expectedX, expectedY));
     }
 
-    private class MarsRover {
-
-        private Direction facing = Direction.NORTH;
-
-        public void sendCommands(String commands) {
-
-            for (char command : commands.toCharArray()) {
-                if (command == 'r') {
-                    facing = Direction.EAST;
-                }
-
-            }
-
-        }
-
-        public Direction facing() {
-            return facing;
-        }
-
-        public Position position() {
-            return new Position(0, 0);
-        }
-    }
 }
