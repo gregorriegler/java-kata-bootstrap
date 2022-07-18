877c6c131106b2553d140e0e60fff0cd252e1625
Gregor Riegler <gregor.riegler@ebcont.com>
2022-07-18 22:55:51 +0200

    wip

--- src/test/java/tdd/MarsRoverTest.java
+++ src/test/java/tdd/MarsRoverTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.Objects;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MarsRoverTest {
@@ -37,34 +35,6 @@ public class MarsRoverTest {
         EAST,
         SOUTH,
         WEST,
-    }
-
-    private static class Position {
-        private final int x;
-        private final int y;
-
-        public Position(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public String toString() {
-            return "(" + x + "," + y + ")";
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Position position = (Position) o;
-            return x == position.x && y == position.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
     }
 
     private class MarsRover {
