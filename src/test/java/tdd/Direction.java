0eb0ec222bcd52f4e0e28a69437230b0b6916a5f
Gregor Riegler <gregor.riegler@ebcont.com>
2022-07-18 23:00:26 +0200

    wip

--- src/test/java/tdd/Direction.java
+++ src/test/java/tdd/Direction.java
@@ -11,7 +11,7 @@ public enum Direction {
         if(this.ordinal() == Direction.values().length-1) {
             return Direction.values()[0];
         }
-        int i = this.ordinal() + 1;
-        return Direction.values()[i];
+        int index = this.ordinal() + 1;
+        return Direction.values()[index];
     }
 }
