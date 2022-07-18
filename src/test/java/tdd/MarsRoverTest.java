9d489f4c4eba9db281c1f871333adaf3ff467137
Gregor Riegler <gregor.riegler@ebcont.com>
2022-07-18 22:47:31 +0200

    wip

--- src/test/java/tdd/MarsRoverTest.java
+++ src/test/java/tdd/MarsRoverTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Objects;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MarsRoverTest {
