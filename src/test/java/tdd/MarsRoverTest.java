4cdf22a0f20c468f2b2df4e33c5c5acc87e70913
Gregor Riegler <gregor.riegler@ebcont.com>
2022-07-18 22:45:33 +0200

    wip

--- src/test/java/tdd/MarsRoverTest.java
+++ src/test/java/tdd/MarsRoverTest.java
@@ -2,6 +2,8 @@ package tdd;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Objects;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MarsRoverTest {
@@ -11,16 +13,50 @@ public class MarsRoverTest {
         var rover = new MarsRover();
 
         assertThat(rover.facing()).isEqualTo(Direction.NORTH);
+        assertThat(rover.position()).isEqualTo(new Position(0, 0));
+
+    }
+
+    private enum Direction {
+        NORTH
+
+    }
+
+    private static class Position {
+        private final int x;
+        private final int y;
+
+        public Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + x + "," + y + ")";
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Position position = (Position) o;
+            return x == position.x && y == position.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
     }
 
     private class MarsRover {
         public Direction facing() {
             return Direction.NORTH;
         }
-    }
-    
-    private enum Direction {
-        NORTH
 
+        public Position position() {
+            return new Position(0, 0);
+        }
     }
 }
