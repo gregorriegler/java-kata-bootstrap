5c0a4e6ac05cf02230d6379e4a2d698cab0b503c
Gregor Riegler <gregor.riegler@ebcont.com>
2022-07-22 23:52:08 +0200

    wip

--- src/test/java/tdd/MarsRoverTest.java
+++ src/test/java/tdd/MarsRoverTest.java
@@ -77,6 +77,7 @@ public class MarsRoverTest {
             assertThat(rover.facing()).isEqualTo(expectedFacing);
             assertThat(rover.position()).isEqualTo(new Position(expectedX, expectedY));
         }
+        
         @DisplayName("backwards")
         @ParameterizedTest(name = "rover moving \"{0}\" faces {1} at ({2},{3})")
         @CsvSource({
