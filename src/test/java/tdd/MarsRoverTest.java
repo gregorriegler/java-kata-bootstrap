c5b643d25eb6d0ee26af66409167bf89b69bd1eb
Gregor Riegler <gregor.riegler@ebcont.com>
2022-07-23 00:11:30 +0200

    wip

--- src/test/java/tdd/MarsRoverTest.java
+++ src/test/java/tdd/MarsRoverTest.java
@@ -72,6 +72,7 @@ public class MarsRoverTest {
             
             "rf,EAST,1,0",
             "rff,EAST,2,0",
+            "rff,EAST,2,0",
         })
         public void forward(String commands, Direction expectedFacing, int expectedX, int expectedY) {
             var rover = new MarsRover();
