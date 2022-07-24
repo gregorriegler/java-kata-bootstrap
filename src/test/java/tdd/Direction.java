9b8b421af500d939e43c2a8ec4478aa9add4c0f2
Gregor Riegler <gregor.riegler@ebcont.com>
2022-07-24 18:08:30 +0200

    wip

--- src/test/java/tdd/Direction.java
+++ src/test/java/tdd/Direction.java
@@ -4,7 +4,7 @@ public enum Direction {
     NORTH(new Position(1, 0)),
     EAST(new Position(1, 0)),
     SOUTH(new Position(0, 1)),
-    WEST(new Position(1, 0)),
+    WEST(new Position(-1, 0)),
     ;
 
     public final Position vector;
