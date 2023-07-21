2541b2ac16dc904f8570a0c0ebc2c922cb865ca4
Gregor Riegler <gregor.riegler@ebcont.com>
2023-07-21 12:28:44 +0200

    refactor

--- src/test/java/TennisGame.java
+++ src/test/java/TennisGame.java
@@ -1,5 +1,5 @@
 public class TennisGame {
-    private int player1Scored;
+    private int player1Points;
     private boolean anyPlayerScored;
 
     public TennisGame(String playerName1, String playerName2) {
@@ -7,19 +7,19 @@ public class TennisGame {
 
     public void wonPoint(String playerName) {
         if (playerName.equals("player1")) {
-            player1Scored++;
+            player1Points++;
         }
         anyPlayerScored = true;
     }
 
     public String getScore() {
-        if (player1Scored > 1) return score("Forty", "Thirty");
-        if (player1Scored == 1) return score("Fifteen", "Thirty");
+        if (player1Points > 1) return score("Forty", "Thirty");
+        if (player1Points == 1) return score("Fifteen", "Thirty");
         if (anyPlayerScored) return "Win for player2";
         return score("Love", "All");
     }
 
-    private String score(String forty, String thirty) {
-        return forty + "-" + thirty;
+    private String score(String player1Score, String player2Score) {
+        return player1Score + "-" + player2Score;
     }
 }
