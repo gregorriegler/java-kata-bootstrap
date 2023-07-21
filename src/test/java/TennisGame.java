a35bd1fabe97f1eeb146dfa909c63813f0344887
Gregor Riegler <gregor.riegler@ebcont.com>
2023-07-21 12:20:17 +0200

    Refactor

--- src/test/java/TennisGame.java
+++ src/test/java/TennisGame.java
@@ -1,20 +1,20 @@
 public class TennisGame {
-    private boolean field1;
-    private boolean field2;
+    private boolean player1Scored;
+    private boolean anyPlayerScored;
 
     public TennisGame(String playerName1, String playerName2) {
     }
 
     public void wonPoint(String playerName) {
         if (playerName.equals("player1")) {
-            field1 = true;
+            player1Scored = true;
         }
-        field2 = true;
+        anyPlayerScored = true;
     }
 
     public String getScore() {
-        if(field1) return "Forty-Thirty";
-        if(field2) return "Win for player2";
+        if (player1Scored) return "Forty-Thirty";
+        if (anyPlayerScored) return "Win for player2";
         return "Love-All";
     }
 }
