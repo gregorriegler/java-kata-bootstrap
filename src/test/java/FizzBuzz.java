1bdda0b931cc0f5fe9d7eafd276c440248cd95d5
Gregor Riegler <gregor.riegler@ebcont.com>
2023-07-21 10:41:11 +0200

    extract singleElement()

--- src/test/java/FizzBuzz.java
+++ src/test/java/FizzBuzz.java
@@ -6,17 +6,20 @@ public class FizzBuzz {
     public String run() {
         var sb = new StringBuilder();
         rangeClosed(1, 20).forEach(i -> {
-            String s;
-            if (i % 3 == 0) {
-                s = "Fizz";
-                //sb.append("Fizz");
-            } else {
-                s = Integer.toString(i);
-                //sb.append(i);
-            }
+            String s = singleElement(i);
             sb.append(s);
             sb.append("-");
         });
         return sb.toString();
+    }
+
+    private String singleElement(int i) {
+        String s;
+        if (i % 3 == 0) {
+            s = "Fizz";
+        } else {
+            s = Integer.toString(i);
+        }
+        return s;
     }
 }
