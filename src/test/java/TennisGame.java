4c2c3753c6a971917d9b95ba94c4d352676c9ce9
Gregor Riegler <gregor.riegler@ebcont.com>
2023-07-21 12:05:05 +0200

    LoveAll

---
+++ src/test/java/TennisGame.java
@@ -0,0 +1,11 @@
+public class TennisGame {
+    public TennisGame(String playerName1, String playerName2) {
+    }
+
+    public void wonPoint(String playerName) {
+    }
+
+    public String getScore() {
+        return "Love-All";
+    }
+}
