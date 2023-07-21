5997a313d02f46a2a514b1b6101f97af3859f43d
Gregor Riegler <gregor.riegler@ebcont.com>
2023-07-21 13:08:59 +0200

    Reorder blocks.

--- src/test/java/TennisGame.java
+++ src/test/java/TennisGame.java
@@ -23,28 +23,32 @@ public class TennisGame {
         final String fifteen = "Fifteen";
         final String thirty = "Thirty";
 
-
-        if (player1Points == 1 && player2Points == 0) {
-            return score(fifteen, love);
-        }
-        if (player1Points == 1 && player2Points == 2) {
-            return score(fifteen, thirty);
-        }
-
         if (player1Points == 3 && player2Points == 4) {
             return "Advantage " + player2Name;
         }
+        if (player1Points == 0 && player2Points == 4) {
+            return "Win for " + player2Name;
+        }
+        
+        if (player1Points == 1 && player2Points == 0) {
+            return format(fifteen, love);
+        }
+        
+        if (player1Points == 1 && player2Points == 2) {
+            return format(fifteen, thirty);
+        }
+
         if (player1Points == 3) {
-            return score("Forty", thirty);
+            return format("Forty", thirty);
         }
         if (player2Points == 0) {
-            return score(love, "All");
+            return format(love, "All");
         }
 
-        return "Win for " + player2Name;
+        return "";
     }
 
-    private String score(String player1Score, String player2Score) {
+    private String format(String player1Score, String player2Score) {
         return player1Score + "-" + player2Score;
     }
 }
