a88a40a6d3ef17ee2c50cd705fc8acd1aab7ae55
Gregor Riegler <gregor.riegler@ebcont.com>
2023-07-21 13:03:41 +0200

    allow reordering of if branches

--- src/test/java/TennisGame.java
+++ src/test/java/TennisGame.java
@@ -23,18 +23,19 @@ public class TennisGame {
         final String fifteen = "Fifteen";
         final String thirty = "Thirty";
 
+
+        if (player1Points == 1 && player2Points == 0) {
+            return score(fifteen, love);
+        }
+        if (player1Points == 1 && player2Points == 2) {
+            return score(fifteen, thirty);
+        }
+
         if (player1Points == 3 && player2Points == 4) {
             return "Advantage " + player2Name;
         }
         if (player1Points == 3) {
             return score("Forty", thirty);
-        }
-
-        if (player1Points == 1 && player2Points > 0) {
-            return score(fifteen, thirty);
-        }
-        if (player1Points == 1) {
-            return score(fifteen, love);
         }
         if (player2Points == 0) {
             return score(love, "All");
